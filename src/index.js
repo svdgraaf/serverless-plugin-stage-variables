b59f9b8627db0ead723d91b68ab3c22503a7a7c0
Sander van de Graaf <mail@svdgraaf.nl>
2016-09-21 16:52:19 +0200

    initial import

---
+++ src/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const _ = require('lodash');
+
+module.exports = Class.extend({
+
+  init: function(serverless, opts) {
+    this._serverless = serverless;
+    this._opts = opts;
+
+    this.hooks = {
+      'before:deploy:deploy': this.addStageVariables.bind(this),
+    };
+  },
+
+  addStageVariables: function() {
+    var template = this._serverless.service.provider.compiledCloudFormationTemplate;
+
+    config = {
+      StageName: 'Foobar',
+      StageDescription: {
+        StageName: 'Foobar',
+        Variables: {
+          "Foo": "Bar"
+        }
+      }
+    }
+
+    // find the deployment, and add the stage variables
+    Object.keys(template.Resources).forEach(function(key){
+      if (template.Resources[key]['Type'] == 'AWS::ApiGateway::Deployment') {
+        template.Resources[key] = _.merge(template.Resources[key], config);
+      }
+    })
+
+    console.log(template.Resources);
+  },
+});
